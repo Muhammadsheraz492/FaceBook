98e8af66a9882340b68a833bc7dde966158d9bfb
Muhammadsheraz492 <90733116+Muhammadsheraz492@users.noreply.github.com>
2021-11-21 10:46:55 +0500

    Splash Screen Started

--- Screens/Splash.js
+++ Screens/Splash.js
@@ -2,12 +2,12 @@ import React from "react";
 import { View, Text, KeyboardAvoidingView } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { SvgUri } from "react-native-svg";
-import Google from "../Svges/google-icon.svg";
+import Facebook from "../Svges/Facebook.svg";
 const Splash = () => {
   return (
     <View style={{ flex: 1 }}>
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Google width={120} height={40} />
+        <Facebook width={120} height={40} />
       </View>
     </View>
   );
